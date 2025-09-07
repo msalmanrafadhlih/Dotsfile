84c1c5630cf78517c21657513605ed161252dacf
msalmanrafadhlih <msalmanrafadhlih@gmail.com>
2025-09-08 01:12:41 +0700

    .

--- flake.nix
+++ flake.nix
@@ -14,7 +14,6 @@
 			system = "x86_64-linux";
 			modules = [
 				./hosts/configuration.nix
-				./hosts/hardware-configuration.nix
 				home-manager.nixosModules.home-manager
 				{
 					home-manager = {
