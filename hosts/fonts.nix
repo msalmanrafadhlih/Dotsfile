97780e2d242fb24510ce2466b95d74b317182da6
msalmanrafadhlih <msalmanrafadhlih@gmail.com>
2025-09-10 16:32:13 +0700

    FONTS

--- hosts/fonts.nix
+++ hosts/fonts.nix
@@ -2,38 +2,42 @@
 
 {
   config = {
-	fonts = {
-	
-		enableDefaultPackages = true;
-		fontconfig = {
-			enable = true;
-			defaultFonts = {
-				monospace = [ "FiraCode Nerd Font" ];
-				sansSerif = [ "Noto Sans" ];
-				serif = [ "Noto Serif" ];
-				emoji = [ "Noto Color Emoji" ];
-			};
-		};
+    fonts = {
+      enableDefaultPackages = true;
 
-		## FONTS PACKAGES
-		packages = with pkgs;
-		[
-			jetbrains-mono
-			# Font dasar
-			dejavu_fonts
-			liberation_ttf
-			noto-fonts
-			noto-fonts-cjk-sans
-			noto-fonts-emoji
+      fontconfig = {
+        enable = true;
+        defaultFonts = {
+          monospace = [ "FiraCode Nerd Font" "JetBrainsMono Nerd Font" ];
+          sansSerif = [ "Noto Sans" "DejaVu Sans" ];
+          serif = [ "Noto Serif" ];
+          emoji = [ "Noto Color Emoji" ];
+        };
+      };
 
-			# Untuk polybar, rofi, terminal (ikon glyph)
-			nerdfonts
+      packages = with pkgs; [
+        # Font dasar
+        dejavu_fonts
+        liberation_ttf
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
 
-			# (opsional) tambahan lain
-			font-awesome
-			material-design-icons
-			termsyn
-		];
-	};
+        # Monospace populer
+        jetbrains-mono
+        nerd-fonts.fira-code
+        nerd-fonts.jetbrains-mono
+
+        # Untuk Polybar / Rofi / terminal (ikon glyph)
+        font-awesome
+        material-design-icons
+        termsyn
+
+        # Tambahan opsional (bisa dipilih)
+        nerd-fonts.hack
+        nerd-fonts.ubuntu-mono
+        nerd-fonts.source-code-pro
+      ];
+    };
   };
 }
