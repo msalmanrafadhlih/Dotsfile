0ea34aa22662cadd7f4fa044e922ceec17762b09
msalmanrafadhlih <msalmanrafadhlih@gmail.com>
2025-10-15 20:47:35 +0700

    SAVE

--- module/bash.nix
+++ module/bash.nix
@@ -5,6 +5,11 @@
         enable = true;
         shellAliases = import ./aliases.nix;
         initExtra = ''
+# plugins
+source ~/.config/bash/plugins/colored-man-pages/colored-man-pages.plugin.sh
+source ~/.config/bash/plugins/fzf/fzf.plugin.sh
+source ~/.config/bash/plugins/progress/progress.plugin.sh
+
 PROMPT_COMMAND='PS1_CMD1=''$(git branch --show-current 2>/dev/null)'; 
 PS1='\[\e[92m\]\u\[\e[0m\] \[\e[38;5;244;2;5m\]\s\[\e[0m\]in \[\e[93m\]\w\[\e[0m\] \$ ~ \[\e[91m\]''${PS1_CMD1}\n\[\e[0m\]'
 
