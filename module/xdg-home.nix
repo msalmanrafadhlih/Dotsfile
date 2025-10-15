3d45706cdd4e638765487476d78c58422823b28f
msalmanrafadhlih <msalmanrafadhlih@gmail.com>
2025-10-15 15:09:28 +0700

    SAVR

--- module/xdg-home.nix
+++ module/xdg-home.nix
@@ -5,19 +5,21 @@
     enable = true;
 
     # Aplikasi default untuk jenis MIME tertentu
-    defaultApplications = {
-      "x-scheme-handler/http" = [ "vivaldi-stable.desktop" ];
-      "x-scheme-handler/https" = [ "vivaldi-stable.desktop" ];
-
-      "application/octet-stream" = [ "geany.desktop" ];
-      "image/png" = [ "com.interversehq.qView.desktop" ];
-      "image/jpeg" = [ "com.interversehq.qView.desktop" ];
-      "image/svg+xml" = [ "com.interversehq.qView.desktop" ];
-      "application/x-zerosize" = [ "geany.desktop" ];
-    };
+#    defaultApplications = {
+#      "x-scheme-handler/http" = [ "vivaldi-stable.desktop" ];
+#      "x-scheme-handler/https" = [ "vivaldi-stable.desktop" ];
+#
+#      "application/octet-stream" = [ "geany.desktop" ];
+#      "image/png" = [ "com.interversehq.qView.desktop" ];
+#      "image/jpeg" = [ "com.interversehq.qView.desktop" ];
+#      "image/svg+xml" = [ "com.interversehq.qView.desktop" ];
+#      "application/x-zerosize" = [ "geany.desktop" ];
+#    };
 
     # Asosiasi tambahan (tidak menggantikan default)
     associations.added = {
+      "x-scheme-handler/http" = [ "vivaldi-stable.desktop" ];
+      "x-scheme-handler/https" = [ "vivaldi-stable.desktop" ];
       "application/octet-stream" = [ "geany.desktop" ];
       "image/gif" = [ "com.interversehq.qView.desktop" ];
       "image/png" = [ "com.interversehq.qView.desktop" ];
