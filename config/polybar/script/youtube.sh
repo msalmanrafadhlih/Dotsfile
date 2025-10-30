44fc49945682079a41fdd13c48370d6eefa9c7c1
msalmanrafadhlih <msalmanrafadhlih@gmail.com>
2025-10-30 18:55:02 +0700

    SAVE

--- config/polybar/script/youtube.sh
+++ config/polybar/script/youtube.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kdocker -f -q chromium --user-data-dir=$HOME/.config/vivaldi/local --app="https://www.youtube.com" --class=youtube &
+kdocker -f -q chromium --user-data-dir=$HOME/.config/vivaldi/youtube --app="https://www.youtube.com" --class=youtube &
 
 #surf "https://chat.openai.com"
